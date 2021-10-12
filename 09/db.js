e908d26c5bf5e1ca1a4a41e9a6863cc4ea82d9af
chenxinglong <xinglong.chen@fenrir-inc.com.cn>
2021-10-12 16:05:55 +0800

    分类-错误处理中间件

---
+++ 09/db.js
@@ -0,0 +1,21 @@
+// 封装db模块进行统一管理与使用
+const fs = require('fs')
+// 避免回调地狱，我们使用express自带的util工具promisify
+const { promisify } = require('util')
+const path = require('path')
+
+// 这样一来我们的readFile方法就是Promise方法了
+const readFile = promisify(fs.readFile)
+const writeFile = promisify(fs.writeFile)
+
+const dbPath = path.join(__dirname, './db.json')
+
+exports.getDb = async () => {
+    const data = await readFile(dbPath, 'utf-8')
+    return JSON.parse(data)
+};
+
+exports.saveDb = async (db) => {
+    const data = JSON.stringify(db, null, '  ')
+    await writeFile(dbPath, data)
+}
